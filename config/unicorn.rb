d02b62bfb40ab60a758d9648c36fe0e5006a40d2
yaonie084 <yaonie084@gmail.com>
2014-08-22 11:19:28 +0800

    增加unicorn 配置

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/ubuntu/wangyu/dianwaimai"
+pid "/home/ubuntu/wangyu/dianwaimai" + "/tmp/pids/unicorn.pid"
+stderr_path "/home/ubuntu/wangyu/dianwaimai" + "/log/unicorn_error.log"
+stdout_path "/home/ubuntu/wangyu/dianwaimai" + "/log/unicorn_access.log"
+
+listen "/tmp/unicorn_call_lunch.sock"
+worker_processes 2
+timeout 30
